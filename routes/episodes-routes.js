304c26b59cc0dc3ff8856b805cc0502c81c9da0b
Kelly Schumacher <kschumacher93@gmail.com>
2023-11-22 16:38:58 -0500

    seeded and migrated mysql with knex, created join table for episode id comments

---
+++ routes/episodes-routes.js
@@ -0,0 +1,22 @@
+// const express = require('express');
+// const router = express.Router();
+// const episodeController = require ('../controllers/episodes-controller')
+
+
+// // ******GET/POST API FOR ALL episodes******
+// router
+//     .route('/')
+//     .get(episodeController.index)
+//     .post(episodeController.add)
+
+// // ******GET/PUT/DELETE API FOR A SINGLE episode card******
+// router
+//     .route('/:id')
+//     .get(episodeController.find)
+//     .delete(episodeController.remove)
+//     .put(episodeController.update)
+    
+
+
+
+module.exports = router;
---
+++ routes/users-routes.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const router = express.Router();
+// const userController = require("../controllers/users-controller");
+
+// router
+//     .route("/")
+//     .get(userController.index)
+//     .post(userController.add)
+
+// router
+//     .route("/:id")
+//     .get(userController.search)
+//     .put(userController.edit)
+//     .delete(userController.remove);
+
+// module.exports = router;
